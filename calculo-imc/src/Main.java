083970d2b13830da486d0f0c89505c991d48c844
GuilhermeGoes <guilhermegoes76@hotmail.com>
2023-01-11 22:35:58 -0300

    FEAT: imc project

---
+++ calculo-imc/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        double altura = 1.75;
+        double peso = 71.5;
+
+        double imc;
+
+        imc = peso / (altura * altura);
+
+        System.out.println(imc);
+    }
+}
