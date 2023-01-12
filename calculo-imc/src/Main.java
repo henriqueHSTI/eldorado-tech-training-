50cc2b878f4e4ee63f6391f2ac2d6eaf8e7ede64
jefferson.siqueira <jefferson08jsa@hotmail.com>
2023-01-11 22:06:14 -0300

    feature: criando classe main para calculo de IMC

---
+++ calculo-imc/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        double altura = 1.85;
+        double peso = 83.0;
+        double imc;
+
+        imc = peso / (altura * altura);
+
+        System.out.println(imc);
+    }
+}
