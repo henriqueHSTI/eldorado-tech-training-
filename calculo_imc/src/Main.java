3202a6baffd5182495241142996f0b5ce8d53592
Jehan Lucas Vieira e Silva <jehan@ciandt.com>
2023-01-11 22:03:26 -0300

    Calculo IMC

---
+++ calculo_imc/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+        double altura = 1.70;
+        double peso = 70;
+
+        double imc = 0;
+
+        imc = peso/ (altura*altura);
+
+
+        System.out.println(imc);
+    }
+}
