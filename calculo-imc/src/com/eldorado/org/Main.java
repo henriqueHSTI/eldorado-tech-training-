ab942e4358534ad388ab2b088555835799be8507
Jeison Fappi <jeison.sr4862@gmail.com>
2023-01-12 19:31:24 -0300

    Exercicio IMC

---
+++ calculo-imc/src/com/eldorado/org/Main.java
@@ -0,0 +1,16 @@
+package com.eldorado.org;
+
+import java.util.logging.Logger;
+
+public class Main {
+    public static void main(String[] args) {
+        double altura = 1.80;
+        double peso = 85.8;
+        double imc;
+
+        imc = peso / (altura * altura);
+        System.out.println(imc);
+
+        Logger.g
+    }
+}
