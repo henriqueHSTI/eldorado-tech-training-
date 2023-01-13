a46972ae460b385f8d7dd3c54089cef835be6c88
mbritzke <matheusbritzke@gmail.com>
2023-01-13 00:14:04 -0300

    Logger adicionado

--- calculo-imc/src/com/eldorado/Main.java
+++ calculo-imc/src/com/eldorado/Main.java
@@ -1,6 +1,12 @@
 package com.eldorado;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class Main {
+
+    private static final Logger LOGGER = Logger.getLogger(Main.class.getName());
+
     public static void main(String[] args) {
 
         double altura = 1.83;
@@ -9,6 +15,16 @@ public class Main {
 
         imc = peso / (altura * altura);
 
-        System.out.println(imc);
+        if (imc < 18.5) {
+            LOGGER.log(Level.INFO, String.format("IMC: %s, Classificacao: %s, Nível de Obesidade: %s", imc, "Magreza", "0"));
+        } else if (imc < 25) {
+            LOGGER.log(Level.INFO, String.format("IMC: %s, Classificacao: %s, Nível de Obesidade: %s", imc, "Normal", "0"));
+        } else if (imc < 30) {
+            LOGGER.log(Level.INFO, String.format("IMC: %s, Classificacao: %s, Nível de Obesidade: %s", imc, "Sobrepeso", "I"));
+        } else if (imc < 40) {
+            LOGGER.log(Level.INFO, String.format("IMC: %s, Classificacao: %s, Nível de Obesidade: %s", imc, "Obesidade", "II"));
+        } else {
+            LOGGER.log(Level.INFO, String.format("IMC: %s, Classificacao: %s, Nível de Obesidade: %s", imc, "Obesidade grave", "III"));
+        }
     }
 }
