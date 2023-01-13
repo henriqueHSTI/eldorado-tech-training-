8dc870d6d43cf59dcc7af7d7e33e97886e86ad20
acss91 <acss_9@hotmail.com>
2023-01-12 22:17:43 -0300

    projeto IMC implementação do Scanner

---
+++ eldorado/src/com/eldorado/calculo/Main.java
@@ -0,0 +1,41 @@
+package com.eldorado.calculo;
+
+import com.eldorado.calculo.modelo.Pessoa;
+import com.eldorado.calculo.utilidades.CalculoImcFerramenta;
+
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Main {
+
+    private static Logger LOGGER= Logger.getLogger(Main.class.getName());
+    public static void main(String[] args) {
+        var calculaImcFerramenta = new CalculoImcFerramenta();
+        var pessoa = new Pessoa();
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite seu Nome");
+        pessoa.setNome(scanner.nextLine());
+
+        System.out.println("Digite sua altura");
+        pessoa.setAltura(scanner.nextDouble());
+
+        System.out.println("Digite seu peso");
+        pessoa.setPeso(scanner.nextDouble());
+
+        System.out.println("Digite susa Idade");
+        pessoa.setIdade(scanner.nextInt());
+
+
+        double imc = calculaImcFerramenta.calculaImc(pessoa);
+
+        LOGGER.log(Level.INFO, pessoa.toString());
+
+        calculaImcFerramenta.printInformacao(imc);
+
+        LOGGER.info(calculaImcFerramenta.retornaNivelObesidade("AAAA"));
+    }
+}
---
+++ eldorado/src/com/eldorado/calculo/modelo/Pessoa.java
@@ -0,0 +1,61 @@
+package com.eldorado.calculo.modelo;
+
+public class Pessoa {
+
+
+        Double altura;
+        Double peso;
+        String nome;
+        String genero;
+        Integer idade;
+
+
+        public double getAltura() {
+            return altura;
+        }
+
+        public void setAltura(Double altura) {
+            this.altura = altura;
+        }
+
+        public double getPeso() {
+            return peso;
+        }
+
+        public void setPeso(Double peso) {
+            this.peso = peso;
+        }
+
+        public String getNome() {
+            return nome;
+        }
+
+        public void setNome(String nome) {
+            this.nome = nome;
+        }
+
+        public String getGenero() {
+            return genero;
+        }
+
+        public void setGenero(String genero) {
+            this.genero = genero;
+        }
+
+        public Integer getIdade() {
+            return idade;
+        }
+
+        public void setIdade(Integer idade) {
+            this.idade = idade;
+        }
+
+        @Override
+        public String toString() {
+            return "Pessoa{" +
+                    "altura=" + altura +
+                    ", nome='" + nome + '\'' +
+                    ", idade=" + idade +
+                    '}';
+        }
+}
