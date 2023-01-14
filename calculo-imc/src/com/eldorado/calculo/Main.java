1c42a109bfd11db88ac70d4cc2a58c72967ec86c
Pablo Herivelton <pherivelton@gmail.com>
2023-01-13 21:20:51 -0300

    Teste User

--- calculo-imc/src/com/eldorado/calculo/Main.java
+++ calculo-imc/src/com/eldorado/calculo/Main.java
@@ -147,7 +147,7 @@ public class Main {
 
             } catch (Exception e) {
 
-                LOGGER.log(Level.SEVERE, "Nome Inválido");
+                LOGGER.log(Level.SEVERE, "Nome Inválido.");
             }
 
         }
